091fcd2e032824f1ecd546a62bdbdc479a9fedf9
Anjan Talatam <tspa2117@gmail.com>
2024-04-16 20:48:47 +0530

    feat: simulate apply now

--- src/modules/SectionOne/index.tsx
+++ src/modules/SectionOne/index.tsx
@@ -1,21 +1,36 @@
 import { useState } from "react";
 import InputEndIcon from "../../components/InputEndIcon";
-import { validatePhoneNumber } from "../../utils";
+import {
+  IApiResponse,
+  simulateApplyNow,
+  validatePhoneNumber,
+} from "../../utils";
 
 function SectionOne() {
   const [phoneNumber, setPhoneNumber] = useState("");
   const [consentChecked, setConsentChecked] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isApplied, setIsApplied] = useState(false);
 
   const handlePhoneNumberChange: React.InputHTMLAttributes<HTMLInputElement>["onChange"] =
     (e) => {
       setPhoneNumber(e.target.value);
     };
 
-  const handleApplyNow: React.DOMAttributes<HTMLFormElement>["onSubmit"] = (
-    e
-  ) => {
-    e.preventDefault();
-  };
+  const handleApplyNow: React.DOMAttributes<HTMLFormElement>["onSubmit"] =
+    async (e) => {
+      e.preventDefault();
+      try {
+        setIsLoading(true);
+        const result: IApiResponse = await simulateApplyNow(phoneNumber);
+        console.log(result.message);
+        setIsApplied(true);
+      } catch (e) {
+        console.log("Error:", e);
+      } finally {
+        setIsLoading(false);
+      }
+    };
 
   const handleClear = () => {
     setPhoneNumber("");
@@ -50,9 +65,7 @@ function SectionOne() {
                 xmlns="http://www.w3.org/2000/svg">
                 <path
                   d="m6 0 1.273 4.727L12 6 7.273 7.273 6 12 4.727 7.273 0 6l4.727-1.273L6 0Z"
-                  fill="#000"
-                  // style="mix-blend-mode:darken"
-                ></path>
+                  fill="#000"></path>
               </svg>
               5x Rewards
               <svg
@@ -63,9 +76,7 @@ function SectionOne() {
                 xmlns="http://www.w3.org/2000/svg">
                 <path
                   d="m6 0 1.273 4.727L12 6 7.273 7.273 6 12 4.727 7.273 0 6l4.727-1.273L6 0Z"
-                  fill="#000"
-                  // style="mix-blend-mode:darken"
-                ></path>
+                  fill="#000"></path>
               </svg>
               Zero Forex Markup
             </p>
@@ -74,82 +85,74 @@ function SectionOne() {
           <div className="hidden md:block">
             <div className="flex justify-between items-center block max-w-[94vw]">
               <div className="flex flex-col">
-                <form onSubmit={handleApplyNow}>
-                  <div className="flex bg-black p-1 pl-2 rounded-xl justify-between">
-                    <div className="flex items-center">
-                      <input
-                        className="bg-black border-0 outline-none text-white p-1 placeholder-[#7E8587] w-44"
-                        placeholder="Enter Phone Number"
-                        value={phoneNumber}
-                        onChange={handlePhoneNumberChange}
-                        maxLength={10}
-                      />
-                      <span className="w-6 flex items-center justify-end h-full">
-                        <InputEndIcon
-                          isValidPhone={isValidPhone}
-                          phoneNumber={phoneNumber}
-                          handleClear={handleClear}
+                {!isApplied && (
+                  <form onSubmit={handleApplyNow}>
+                    <div className="flex bg-black p-1 pl-2 rounded-xl justify-between">
+                      <div className="flex items-center">
+                        <input
+                          className="bg-black border-0 outline-none text-white p-1 placeholder-[#7E8587] w-44"
+                          placeholder="Enter Phone Number"
+                          value={phoneNumber}
+                          onChange={handlePhoneNumberChange}
+                          maxLength={10}
                         />
-                        {/* {phoneNumber && (
-                          <button type="button" onClick={handleClear}>
-                            <svg
-                              width="4"
-                              height="17"
-                              fill="none"
-                              xmlns="http://www.w3.org/2000/svg">
-                              <path
-                                d="M2.688 11.664 3.336 0H.048l.648 11.664h1.992Zm-.984 5.328c.96 0 1.68-.72 1.68-1.68s-.72-1.68-1.68-1.68c-.984 0-1.704.72-1.704 1.68s.72 1.68 1.704 1.68Z"
-                                fill="#F48282"></path>
-                            </svg>
-                          </button>
-                        )} */}
-                      </span>
+                        <span className="w-6 flex items-center justify-end h-full">
+                          <InputEndIcon
+                            isValidPhone={isValidPhone}
+                            phoneNumber={phoneNumber}
+                            handleClear={handleClear}
+                          />
+                        </span>
+                      </div>
+                      <button
+                        type="submit"
+                        className="text-center text-sm leading-7 justify-between pt-2 ml-2 bg-uni-yellow rounded-xl z-10 py-2 px-4 disabled:opacity-80 disabled:cursor-not-allowed bg-[#fdef78]"
+                        disabled={
+                          (!isValidPhone && phoneNumber != "") ||
+                          !consentChecked
+                        }>
+                        <span>{isLoading ? "Applying..." : "Apply Now"}</span>
+                      </button>
                     </div>
-                    <button
-                      type="submit"
-                      className="text-center text-sm leading-7 justify-between pt-2 ml-2 bg-uni-yellow rounded-xl z-10 py-2 px-4 disabled:opacity-80 disabled:cursor-not-allowed bg-[#fdef78]"
-                      disabled={
-                        (!isValidPhone && phoneNumber != "") || !consentChecked
-                      }>
-                      <span>Apply Now</span>
-                    </button>
+                    <div className="flex items-center py-4 px-2 max-w-xs gap-2">
+                      <input
+                        type="checkbox"
+                        checked={consentChecked}
+                        onChange={(e) => setConsentChecked(e.target.checked)}
+                        id="consent-msg"
+                      />
+                      <label
+                        htmlFor="consent-msg"
+                        className="consent text-white md:text-black text-[10px] leading-3 cursor-pointer">
+                        You agree to be contacted by Uni Cards over Call, SMS,
+                        Email or WhatsApp to guide you through your application.
+                      </label>
+                    </div>
+                  </form>
+                )}
+                {isApplied && (
+                  <div className="flex max-w-xs flex-col justify-start">
+                    <div className="w-full max-w-[300px]">
+                      <a
+                        href="https://uni-growth.onelink.me/v6cm/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="block google-btn-2 font-medium p-4 rounded-lg z-10 bg-[#FDEF78] text-black -mt-2 disabled:bg-[#AEAB8C]">
+                        <div className="w-full flex justify-center items-center">
+                          <span>Download</span>
+                        </div>
+                      </a>
+                    </div>
+                    <div className="my-2">
+                      <p className="text-white md:text-black text-[10px] leading-3">
+                        Thank you for your interest in the Uni Card.
+                        <br /> Download the Uni Cards app now and get your Uni
+                        Card in minutes.
+                      </p>
+                    </div>
                   </div>
-                </form>
-                <div className="flex items-center py-4 px-2 max-w-xs gap-2">
-                  <input
-                    type="checkbox"
-                    checked={consentChecked}
-                    onChange={(e) => setConsentChecked(e.target.checked)}
-                    id="consent-msg"
-                  />
-                  <label
-                    htmlFor="consent-msg"
-                    className="consent text-white md:text-black text-[10px] leading-3 cursor-pointer">
-                    You agree to be contacted by Uni Cards over Call, SMS, Email
-                    or WhatsApp to guide you through your application.
-                  </label>
-                </div>
+                )}
               </div>
-            </div>
-            <div className="flex max-w-xs flex-col justify-start">
-              <div className="w-full max-w-[300px]">
-                {/* <a
-                    href="https://uni-growth.onelink.me/v6cm/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="block google-btn-2 font-medium p-4 rounded-lg z-10 bg-[#FDEF78] text-black -mt-2 disabled:bg-[#AEAB8C]">
-                    <div className="w-full flex justify-center items-center">
-                      <span>Download</span>
-                    </div>
-                  </a> */}
-              </div>
-              {/* <div className="my-2">
-                  <p className="text-white md:text-black text-[10px] leading-3">
-                    Thank you for your interest in the Uni Card.
-                    <br /> Download the Uni Cards app now and get your Uni Card in
-                    minutes.
-                  </p>
-                </div> */}
             </div>
           </div>
         </div>
--- src/utils.ts
+++ src/utils.ts
@@ -3,3 +3,17 @@ export const MobileRegex = /^\d{10}$/;
 export function validatePhoneNumber(phone: string) {
   return MobileRegex.test(phone);
 }
+
+export interface IApiResponse {
+  status: "success" | "failure";
+  message: string;
+}
+
+export function simulateApplyNow(phone: string) {
+  return new Promise<IApiResponse>((resolve) => {
+    setTimeout(() => {
+      console.log(phone);
+      resolve({ status: "success", message: "Form submitted successfully!" });
+    }, Math.random() * 2500);
+  });
+}
