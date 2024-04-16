856b6fb6291d1313a8ad6cb4e15d4be576ab1e41
Anjan Talatam <tspa2117@gmail.com>
2024-04-16 19:49:56 +0530

    refactor: style changes

--- src/modules/Layout/index.tsx
+++ src/modules/Layout/index.tsx
@@ -9,18 +9,18 @@ function Layout() {
   return (
     <main>
       <header className={"header"}>
-        <nav className="flex items-center justify-between w-full z-15 min-h-[80px] px-4">
+        <nav className="flex items-center justify-between w-full z-15 px-8">
           <img
             alt="uni_logo"
             src="/assets/images/uni_logo.svg"
             loading="lazy"
-            className="w-[85px]"
+            className="w-[115px]"
           />
 
           <a
             href="https://paychek.uni.club/"
             target="_blank"
-            className="opacity-90 z-15 no-underline visited:text-white text-white font-medium hover:text-[#7DEDDD] bg-black bg-opacity-60 hover:bg-black rounded-xl py-[11px] px-[30px]">
+            className="opacity-90 z-15 no-underline visited:text-white text-white font-medium hover:text-[#7DEDDD] bg-black bg-opacity-60 hover:bg-black rounded-xl py-[8px] px-[30px]">
             Uni Paycheck
           </a>
         </nav>
